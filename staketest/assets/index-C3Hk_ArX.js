a369736b1ee18ebe598baf06981dab17a9b82802
Thant Sin Toe <mr.thantsintoe@gmail.com>
2024-12-21 21:05:07 +0700

    second round of stake test UI changes

--- staketest/assets/index-C3Hk_ArX.js
+++ staketest/assets/index-C3Hk_ArX.js
@@ -1,4 +1,4 @@
-import{T as $t,M as oe,R as N,E as k,C as $,a as X,b as Br,O as Z,c as ht}from"./index-CdiOb_PK.js";import{n as Wr}from"./index-B3KYQuZx.js";/**
+import{T as $t,M as oe,R as N,E as k,C as $,a as X,b as Br,O as Z,c as ht}from"./index-5l2aoNyN.js";import{n as Wr}from"./index-DQ32eqN8.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
