083ba5dcc0fc17b3b8a404b2c306cebb7f3dbc49
jairajdev <jairajranamagar.dev@gmail.com>
2024-12-23 19:33:27 +0545

    wc2 update

--- wc2/components/account.js
+++
@@ -1,5 +0,0 @@
-export function accountPage() {
-    return '<h1>Account Page</h1>';
-  }
-  
-  
--- wc2/components/contacts.js
+++
@@ -1,5 +0,0 @@
-export function contactsPage() {
-    return '<h1>Contacts Page</h1>';
-  }
-  
-  
