9424f5784ce443f57fda730a4b75c14cc2a82c11
Thant Sin Toe <mr.thantsintoe@gmail.com>
2024-12-28 22:04:15 +0700

    fix weight in update pair weight modal

--- staketest/assets/index-BZ5zMfwA.js
+++ staketest/assets/index-BZ5zMfwA.js
@@ -1,4 +1,4 @@
-import{T as $t,M as oe,R as N,E as k,C as $,a as X,b as Br,O as Z,c as ht}from"./index-mOKfVYDk.js";import{n as Wr}from"./index-DixDnprT.js";/**
+import{T as $t,M as oe,R as N,E as k,C as $,a as X,b as Br,O as Z,c as ht}from"./index-dC6vOoM1.js";import{n as Wr}from"./index-DuvXXRtc.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
