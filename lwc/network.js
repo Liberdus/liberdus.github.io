4ea42c349968a5507c82c051829c29393c763efa
jairajdev <jairajranamagar.dev@gmail.com>
2025-01-21 09:54:33 +0700

    Added liberdus web client

---
+++ lwc/network.js
@@ -0,0 +1,17 @@
+const network = {
+  netid: "2f4b9f72089bbfce9f89d3d8e76086daab6ae6f416887c809aab26abb6e5703b",
+  gateways: [
+    {
+      protocol: "https",
+      host: "dev.liberdus.com",
+      port: 3030,
+    },
+  ],
+  rpc_server: [
+    {
+      protocol: "https",
+      host: "dev.liberdus.com",
+      port: 8545,
+    },
+  ],
+};
