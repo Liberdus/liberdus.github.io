e684d96f3d6dfb077b1c1ce56cf8980196868f54
dnlbui <dnlbui@proton.me>
2025-01-28 15:36:41 -0600

    Update network configuration

--- lwc/dnl/network.js
+++ lwc/dnl/network.js
@@ -1,10 +1,17 @@
 const network = {
-  "netid": "2f4b9f72089bbfce9f89d3d8e76086daab6ae6f416887c809aab26abb6e5703b",
-  "gateways": [
+  netid: "2f4b9f72089bbfce9f89d3d8e76086daab6ae6f416887c809aab26abb6e5703b",
+  gateways: [
     {
-      "protocol": "http",
-      "host": "localhost",
-      "port": 3030    
+      protocol: "https",
+      host: "dev.liberdus.com",
+      port: 3030,
     },
-  ]
-}
+  ],
+  rpc_server: [
+    {
+      protocol: "https",
+      host: "dev.liberdus.com",
+      port: 8545,
+    },
+  ],
+};
