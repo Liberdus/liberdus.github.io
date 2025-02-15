b67580455371bc224c2ba84946c28d4c46564434
Omar Syed <osyed1@gmail.com>
2025-02-14 22:15:55 -0600

    Update network.js

--- test/network.js
+++ test/network.js
@@ -1,17 +1,18 @@
 const network = {
   netid: "2f4b9f72089bbfce9f89d3d8e76086daab6ae6f416887c809aab26abb6e5703b",
+  "name": "Testnet",
   gateways: [
     {
       protocol: "https",
-      host: "dev.liberdus.com",
+      host: "test.liberdus.com",
       port: 3030,
     },
   ],
-  rpc_server: [
-    {
-      protocol: "https",
-      host: "dev.liberdus.com",
-      port: 8545,
-    },
-  ],
+  "monitor": {
+    "url": "https://test.liberdus.com/monitor"
+  },
+  "explorer": {
+    "url": "https://test.liberdus.com/explorer"
+  }
 };
+
