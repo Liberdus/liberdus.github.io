997fccacf4ffc1719b625db8f285a24667941500
Omar Syed <osyed1@gmail.com>
2025-04-20 23:44:58 -0500

    Update network.js

--- dev/network.js
+++ dev/network.js
@@ -1,20 +1,20 @@
 const network = {
   "netid": "2f4b9f72089bbfce9f89d3d8e76086daab6ae6f416887c809aab26abb6e5703b",
-  "name": "Testnet",
+  "name": "Devnet",
   "gateways": [
     {
       "protocol": "https",
-      "host": "test.liberdus.com",
+      "host": "dev.liberdus.com",
       "port": 3030    
     },
   ],
   "monitor": {
-    "url": "https://test.liberdus.com/monitor"
+    "url": "https://dev.liberdus.com/monitor"
   },
   "explorer": {
-    "url": "https://test.liberdus.com/explorer"
+    "url": "https://dev.liberdus.com/explorer"
   },
   websocket: {
-    url: "wss://test.liberdus.com:3031",
+    url: "wss://dev.liberdus.com:3031",
   }
 }
