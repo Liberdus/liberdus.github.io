242f55d9da297397999c0e569832aa776fa9e5ef
dnlbui <dnlbui@proton.me>
2025-05-21 13:25:52 -0500

    Update version identifier in app.js to 't', increment resource versioning in index.html and version.html for consistency, and add error toast for WebSocket reconnection to improve user feedback.

--- dev/app.js
+++ dev/app.js
@@ -1,6 +1,6 @@
 // Check if there is a newer version and load that using a new random url to avoid cache hits
 //   Versions should be YYYY.MM.DD.HH.mm like 2025.01.25.10.05
-const version = 's'
+const version = 't'
 let myVersion = '0'
 async function checkVersion(){
     myVersion = localStorage.getItem('version') || '0';
@@ -1168,6 +1168,7 @@ function handleVisibilityChange(e) {
         showToast('App has been shown', 0, 'error');
         // Reconnect WebSocket if needed
         if (wsManager && !wsManager.isConnected() && myAccount) {
+            showToast('Reconnecting WebSocket', 0, 'error');
             wsManager.connect();
         }
     }
