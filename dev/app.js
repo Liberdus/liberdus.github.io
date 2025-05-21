0e28845cf81c4a5f8d88ef7e73209161b5338fbf
dnlbui <dnlbui@proton.me>
2025-05-21 10:26:35 -0500

    Update version identifier in app.js to 'q', increment resource versioning in index.html and version.html for consistency across resources, and comment out WebSocket subscription to prevent unnecessary calls.

--- dev/app.js
+++ dev/app.js
@@ -1,6 +1,6 @@
 // Check if there is a newer version and load that using a new random url to avoid cache hits
 //   Versions should be YYYY.MM.DD.HH.mm like 2025.01.25.10.05
-const version = 'o'
+const version = 'q'
 let myVersion = '0'
 async function checkVersion(){
     myVersion = localStorage.getItem('version') || '0';
@@ -6287,7 +6287,7 @@ function getCorrectedTimestamp() {
 function updateWebSocketIndicator() {
     // added this so that we don't miss messages on phones, since phones drop the ws if not used periodically
     if (getCorrectedTimestamp() - updateWebSocketIndicator.lastSubscribed > 31000){
-        wsManager.subscribe()
+        /* wsManager.subscribe() */
         updateWebSocketIndicator.lastSubscribed = getCorrectedTimestamp()
     }
     const indicator = document.getElementById('wsStatusIndicator');
