f6db5370231cd2a8dd0518a4257b5e9281217313
dnlbui <dnlbui@proton.me>
2025-06-02 17:59:29 -0500

    Update version identifier in app.js to 'r', increment resource versioning in index.html and version.html for consistency, and add additional debug logging in ChatModal to enhance message tracking and troubleshooting.

--- dev/app.js
+++ dev/app.js
@@ -1,6 +1,6 @@
 // Check if there is a newer version and load that using a new random url to avoid cache hits
 //   Versions should be YYYY.MM.DD.HH.mm like 2025.01.25.10.05
-const version = 'p'
+const version = 'r'
 let myVersion = '0'
 async function checkVersion(){
     myVersion = localStorage.getItem('version') || '0';
@@ -7097,8 +7097,8 @@ class ChatModal {
         const contact = myData.contacts[contactAddress];
         const latestMessage = this.newestReceivedMessage;
         console.log('DEBUG2:latestMessage:', latestMessage);
+        console.log('DEBUG3:contact.timestamp < latestMessage.timestamp:', contact.timestamp, latestMessage.timestamp);
         if (contact.timestamp < latestMessage.timestamp) {
-            console.log('DEBUG3:contact.timestamp < latestMessage.timestamp:', contact.timestamp, latestMessage.timestamp);
             const readTransaction = await this.createReadTransaction(contactAddress);
             console.log('DEBUG4:readTransaction:', readTransaction);
             const txid = await signObj(readTransaction, myAccount.keys)
@@ -7379,6 +7379,7 @@ class ChatModal {
         const newestReceivedItem = messages.find(item => !item.my);
         console.log('appendChatModal: Identified newestReceivedItem data:', newestReceivedItem);
         this.newestReceivedMessage = newestReceivedItem;
+        console.log('DEBUG5:[appendChatModal] newestReceivedMessage:', this.newestReceivedMessage);
     
         // 2. Clear the entire list
         this.messagesList.innerHTML = '';
