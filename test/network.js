16b9fa3db8e23ac635de49b987d2810f4f1e95bd
jairajdev <jairajranamagar.dev@gmail.com>
2025-06-27 17:04:34 +0545

    Updated the network configurations

--- test/network.js
+++ test/network.js
@@ -1,10 +1,10 @@
 const network = {
   "netid": "fd1b56b08fd1e5035aa19eb631f7f1ad0395175c5d3dfc49411dfa528e6af7c3",
-  "name": "Devnet",
+  "name": "Testnet",
   "gateways": [
     {
-      "web": "http://localhost:3030",
-      "ws": "ws://localhost:3031"
+      "web": "https://test.liberdus.com:3030",
+      "ws": "wss://test.liberdus.com:3031"
     },
   ],
 }
