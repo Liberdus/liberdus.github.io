58d18e24e5c068c88561870508a81e5245f005c6
Omar Syed <osyed1@gmail.com>
2025-07-12 01:32:50 -0500

    Update network.js

--- test/network.js
+++ test/network.js
@@ -1,5 +1,5 @@
 const network = {
-  "netid": "d76b585f8da6d28555c734144d9c965e6d5cb2d4c7248b92265a926408bfece6",
+  "netid": "fd1b56b08fd1e5035aa19eb631f7f1ad0395175c5d3dfc49411dfa528e6af7c3",
   "name": "Testnet",
   "gateways": [
     {
@@ -8,6 +8,6 @@ const network = {
     },
   ],
   "netids": [
-    "fd1b56b08fd1e5035aa19eb631f7f1ad0395175c5d3dfc49411dfa528e6af7c3", // previous
+    "d76b585f8da6d28555c734144d9c965e6d5cb2d4c7248b92265a926408bfece6", // previous
   ],
 }
