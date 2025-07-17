dab74ebd8c9b17aa10673e3b89d39757d0fda8d2
Thant Sin Toe <mr.thantsintoe@gmail.com>
2025-07-17 11:22:45 +0700

    update dev network IDs for the Devnet in the dev/network.js file

--- dev/network.js
+++ dev/network.js
@@ -1,5 +1,5 @@
 const network = {
-  "netid": "627510f0cb0bf5e82b0cc8bace10a1a3649c74d9b733af9a32e26d495e5799fe",
+  "netid": "7440f5161ffc77eed9ee91d6fbb406083192d1fe4d7e64b2f0814c0e067dcab4",
   "name": "Devnet",
   "gateways": [
     {
@@ -8,6 +8,7 @@ const network = {
     },
   ],
   "netids": [
-    "627510f0cb0bf5e82b0cc8bace10a1a3649c74d9b733af9a32e26d495e5799fe", // current
+    "7440f5161ffc77eed9ee91d6fbb406083192d1fe4d7e64b2f0814c0e067dcab4", // current
+    "627510f0cb0bf5e82b0cc8bace10a1a3649c74d9b733af9a32e26d495e5799fe", // old
   ],
 }
