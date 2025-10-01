31dea1b67c76b7bd4b56352fa3cbc3336431c7f5
dnlbui <dnlbui@proton.me>
2025-10-01 17:12:40 -0500

    DEV: Update app version to 't', improve fullscreen handling on Android devices with a delay, and update version references in HTML and CSS files.

--- dev/app.js
+++ dev/app.js
@@ -1,6 +1,6 @@
 // Check if there is a newer version and load that using a new random url to avoid cache hits
 //   Versions should be YYYY.MM.DD.HH.mm like 2025.01.25.10.05
-const version = 's'
+const version = 't'
 let myVersion = '0';
 async function checkVersion() {
   myVersion = localStorage.getItem('version') || '0';
@@ -16972,7 +16972,10 @@ function enterFullscreen() {
   if (isMobile()) {
   console.log('in enterFullscreen');
     if (document.documentElement.requestFullscreen) {
-      document.documentElement.requestFullscreen();
+      // on android 15 using chrome without delay caused issues with input field on ChatModal to be positioned below visual viewport
+      setTimeout(() => {
+        document.documentElement.requestFullscreen();
+      }, 100);
     } 
   }
 }
